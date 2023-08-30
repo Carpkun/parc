ca6acd0afc12a10f5aaa0438538f50d62d5466c1
Carpkun <rcourse@hanmail.net>
2023-08-30 16:55:46 +0900

    ch8

---
+++ ch8/8-1.Paint_Area_Calculator.py
@@ -0,0 +1,20 @@
+#페인트 면적 계산기
+
+#Write your code below this line 👇
+import math
+
+def paint_calc(height, width, cover):
+    area = height * width
+    number_of_cans = math.ceil(area / cover)
+    print(f"You'll need {number_of_cans} cans of paint.")
+
+
+#Write your code above this line 👆
+# Define a function called paint_calc() so that the code below works.   
+
+# 🚨 Don't change the code below 👇
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
+
